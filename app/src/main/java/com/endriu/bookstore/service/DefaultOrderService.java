94572049b80d934fb0e926970069184b8963177d
AndrzejSasak <andrzej.sasak01@gmail.com>
2022-10-28 14:57:55 +0200

    Add getOrderById method to OrderService
    Implement getOrderById method and createOrder method in DefaultOrderService

--- app/src/main/java/com/endriu/bookstore/service/DefaultOrderService.java
+++ app/src/main/java/com/endriu/bookstore/service/DefaultOrderService.java
@@ -2,30 +2,63 @@ package com.endriu.bookstore.service;
 
 import com.endriu.bookstore.domain.Customer;
 import com.endriu.bookstore.domain.Order;
+import com.endriu.bookstore.domain.OrderItem;
 import com.endriu.bookstore.domain.ShoppingCart;
+import com.endriu.bookstore.repository.CustomerRepository;
+import com.endriu.bookstore.repository.OrderItemRepository;
+import com.endriu.bookstore.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class DefaultOrderService implements OrderService {
 
+    private final CustomerRepository customerRepository;
+    private final OrderRepository orderRepository;
+
+    public DefaultOrderService(CustomerRepository customerRepository,
+                               OrderRepository orderRepository) {
+        this.customerRepository = customerRepository;
+        this.orderRepository = orderRepository;
+    }
+
+
     @Override
     public Order createOrder(Customer customer) {
 
-        ShoppingCart shoppingCart = customer.getShoppingCart();
+        if(!customer.hasEnoughBalance()) {
+            throw new NotEnoughFundsException("Not enough funds.");
+        }
 
+        if(!customer.hasOrderItemsInCart()) {
+            throw new IllegalStateException("Cart cannot be empty");
+        }
 
+        List<OrderItem> orderItems = customer.getOrderItems();
 
         Order newOrder = Order.builder()
-                .orderItems(shoppingCart.getOrderItems())
                 .customer(customer)
+                .price(customer.getCartPrice())
                 .timestampCreated(LocalDateTime.now())
-                .price(shoppingCart.getPrice())
+                .orderItems(orderItems)
                 .build();
 
+        orderItems.forEach(orderItem -> orderItem.setOrder(newOrder));
+        customer.addOrder(newOrder);
+        customer.pay(newOrder.getPrice());
 
-        return newOrder;
+        customerRepository.save(customer);
+        return orderRepository.save(newOrder);
+    }
+
+    @Override
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("No order with id: " + id + " found"));
     }
 
 }
